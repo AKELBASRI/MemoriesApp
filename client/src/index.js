f1044de55a7748cc478afada5a4a894889e409e9
Mac <mac@192.168.1.5>
2021-04-22 22:46:18 +0000

    styles appbar grid form

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-ReactDOM.render(<App/>,document.getElementById('root'));
+import {Provider} from 'react-redux'
+import {createStore,applyMiddleware,compose} from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store=createStore(reducers,composeEnhancers(applyMiddleware(thunk)));
+ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+  
--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ const cors = require("cors");
 const postRoutes = require("./routes/posts.js");
 
 const app = express();
-app.use("/posts", postRoutes);
+
 app.use(express.json({ limit: "50mb", extended: true }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
-
+app.use("/posts", postRoutes);
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 mongoose
